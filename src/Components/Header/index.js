aefe227894038aa9ec643e0d9ccf47f49bdb0732
Chet Jogia <chetjogia@gmail.com>
2023-02-08 18:14:19 +0000

    logo added

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -2,15 +2,17 @@ import SearchBar from "../SearchBar";
 import "./index.css";
 import { useRef } from "react";
 import { languages } from "../../countrydata";
+import logo from "../../percaysologo.png";
 function Header({ getNewsData, handleLanguage, getTopNews }) {
   const language = useRef("");
-
 
   return (
     <div className="top-container">
       <div className="header-container">
-        <h1 className="app-title">Percayso Inform News</h1>
-
+        {/*   <h1 className="app-title">Percayso Inform News</h1> */}
+        <div className="logo">
+          <img src={logo} alt="logo"/>
+        </div>
         <nav className="nav-bar">
           <div className="set-region">
             <label>Set Region/Language:</label>
@@ -27,14 +29,54 @@ function Header({ getNewsData, handleLanguage, getTopNews }) {
         </nav>
       </div>
       <div className="top-stories">
-          <button className="category-button" onClick={() => getTopNews("general", language.current.value)}>Top Stories</button>
-          <button className="category-button" onClick={() => getTopNews("world", language.current.value)}>World</button>
-          <button className="category-button" onClick={() => getTopNews("nation", language.current.value)}>Nation</button>
-          <button className="category-button"  onClick={() => getTopNews("business", language.current.value)}>Business</button>
-          <button className="category-button" onClick={() => getTopNews("entertainment", language.current.value)}>Entertainment</button>
-          <button className="category-button" onClick={() => getTopNews("sports", language.current.value)}>Sports</button>
-          <button className="category-button"  onClick={() => getTopNews("science", language.current.value)}>Science</button>
-          <button className="category-button" onClick={() => getTopNews("health", language.current.value)}>Health</button>
+        <button
+          className="category-button"
+          onClick={() => getTopNews("general", language.current.value)}
+        >
+          Top Stories
+        </button>
+        <button
+          className="category-button"
+          onClick={() => getTopNews("world", language.current.value)}
+        >
+          World
+        </button>
+        <button
+          className="category-button"
+          onClick={() => getTopNews("nation", language.current.value)}
+        >
+          Nation
+        </button>
+        <button
+          className="category-button"
+          onClick={() => getTopNews("business", language.current.value)}
+        >
+          Business
+        </button>
+        <button
+          className="category-button"
+          onClick={() => getTopNews("entertainment", language.current.value)}
+        >
+          Entertainment
+        </button>
+        <button
+          className="category-button"
+          onClick={() => getTopNews("sports", language.current.value)}
+        >
+          Sports
+        </button>
+        <button
+          className="category-button"
+          onClick={() => getTopNews("science", language.current.value)}
+        >
+          Science
+        </button>
+        <button
+          className="category-button"
+          onClick={() => getTopNews("health", language.current.value)}
+        >
+          Health
+        </button>
       </div>
     </div>
   );
